dbc2886af49713737adb2c6f223e257e95fe3123
Joy Mompi <jmompi1@jh.edu>
2025-09-04 14:41:04 -0400

    upload unzipped genome sizes

---
+++ miniprohect-assembly-metrics/getGenomes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA248909/caenorhabditis_remanei.PRJNA248909.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA248911/caenorhabditis_remanei.PRJNA248911.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA53967/caenorhabditis_remanei.PRJNA53967.WBPS19.genomic.fa.gz
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA577507/caenorhabditis_remanei.PRJNA577507.WBPS19.genomic.fa.gz
+
+-rw-r--r--  1 cmdb  staff   115M Mar 21  2024 caenorhabditis_remanei.PRJNA248909.WBPS19.genomic.fa
+-rw-r--r--  1 cmdb  staff   121M Mar 21  2024 caenorhabditis_remanei.PRJNA248911.WBPS19.genomic.fa
+-rw-r--r--  1 cmdb  staff   141M Aug 31  2023 caenorhabditis_remanei.PRJNA53967.WBPS19.genomic.fa
+-rw-r--r--  1 cmdb  staff   127M Aug 31  2023 caenorhabditis_remanei.PRJNA577507.WBPS19.genomic.fa
